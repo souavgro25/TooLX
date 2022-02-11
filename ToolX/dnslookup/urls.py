55693bd6d6ce8727b79a5baa911ca1cae894dae5
souavgro25 <souravgro25@gmail.com>
2022-02-11 18:33:57 +0530

    nmap testing script

--- ToolX/dnslookup/urls.py
+++ ToolX/dnslookup/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('ab/<str:host>/<str:type>', views.nslookup, name='dnslookup'),
-
+    path('about',views.about,name='about'),
+    path('nmap/<str:host>/<str:port>/<int:type>',views.portscanner,name='portscanner')
 ]
--- ToolX/dnslookup/views.py
+++ ToolX/dnslookup/views.py
@@ -1,8 +1,9 @@
-from multiprocessing import context
+
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import dns.resolver
-
+import json
+import nmap 
 
 
 def nslookup(request,host,type):
@@ -10,15 +11,32 @@ def nslookup(request,host,type):
         result = dns.resolver.query(host,type)
     except:
         context={"records":["no data found"],'host':host,'type':type}
-        return JsonResponse({"data":context})
+        return JsonResponse({"data":context ,"error":'1'})
     records = []
     for val in result:
         
         records.append(val.to_text())
     context={ 'records': records ,'host':host,'type':type}
-    return JsonResponse({"data":context})
+    return JsonResponse({"data":context ,"error":"0"})
     
 
 def index(request):
     return render(request,'index.html')
    
+def about(request):
+    return render(request, 'about.html')
+
+def portscanner(request,host, port,type):
+    nmScan = nmap.PortScanner()
+    # try:
+    if (type==1):
+        scan = nmScan.scan(host,arguments='-sV  -p'+port)
+    elif(type==2):
+        scan = nmScan.scan(host,arguments='-sC -p'+port)
+    elif(type==3):
+        scan = nmScan.scan(host,arguments='--privileged -sU -p'+port)
+    elif(type==4):
+        scan = nmScan.scan(host,arguments=' --privileged -sN -p'+port)
+    return JsonResponse({'data':scan,"error":"0"})
+    # except:
+    #     return JsonResponse({'data':"failed to scan this host ","error":"1"})
