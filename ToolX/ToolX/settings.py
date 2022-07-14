4aa6ebecdfd45b93292683f705e1f779b29e4de9
souavgro25 <souravgro25@gmail.com>
2022-07-14 13:18:31 +0530

    db chnages

--- ToolX/ToolX/settings.py
+++ ToolX/ToolX/settings.py
@@ -27,7 +27,7 @@ GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.8', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.0.8', '127.0.0.1','172.28.116.197']
 
 
 # Application definition
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'ToolX.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'projectx',
-        'USER': 'sammy',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
